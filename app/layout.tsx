4faa5ac8b09e7d122947a61d281f42949c493d7d
Hyrox <thehyrox@gmail.com>
2024-06-09 18:06:42 +0200

    Settings Dialog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
+
 import "./globals.css";
+
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
-import { Toaster } from "sonner";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -36,6 +39,7 @@ export default function RootLayout({
             storageKey="jotion-theme"
           >
             <Toaster position="bottom-center"/>
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
---
+++ hooks/use-settings.tsx
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+type Settings = {
+    isOpen: boolean;
+    onOpen: () => void;
+    onClose: () => void;
+};
+
+export const useSettings = create<Settings>((set) => ({
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false }),
+}));
